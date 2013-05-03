182511eeb425714cda18f42a36334242d1569bb2
k-i-t-e <darkmik.kite@gmail.com>
2013-05-03 21:09:18 +0400

    a

--- nova/scheduler/filter_scheduler.py
+++ nova/scheduler/filter_scheduler.py
@@ -60,8 +60,8 @@ class FilterScheduler(driver.Scheduler):
         self.cost_function_cache = None
         self.options = scheduler_options.SchedulerOptions()
         ## My test code
-        self.test_file = open("/home/test_file", "w+")
-        ##self.first_time = True
+        #self.test_file = open("/home/test_file", "w+")
+        self.first_time = True
 
 #    def my_schedule_run_instance(self, context, request_spec,
 #                              admin_password, injected_files,
